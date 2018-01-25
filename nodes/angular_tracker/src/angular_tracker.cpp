6603f7efb6caeb539fe367e49987113b850c5614
Martijn <martijn.posthuma@hva.nl>
2018-01-25 21:37:40 +0100

    Update angular_tracker to filtered version
    Arnout + Martijn

--- nodes/angular_tracker/src/angular_tracker.cpp
+++ nodes/angular_tracker/src/angular_tracker.cpp
@@ -13,42 +13,89 @@ using namespace std;
 
 ros::Publisher driver;
 
-void trackedCallback(const vision::TrackedPositionConstPtr& msg) {
-  ROS_INFO("x: [%f], y: [%f], z: [%f], ", -msg->x * 2, -msg->y, -msg->z);
+double speed;
 
-  double rotation = -msg->x * 2;
+//initial values for the lowpass filter
+double rawData;
+double smoothData;
+double LPF_Beta = 0.5; // 0<ß<1
 
-  geometry_msgs::Twist driveObj;
-  driveObj.angular.z = rotation;
-
-  if(rotation < 1 && rotation > -1) {
-
-    double filter = msg->z;
-    if(filter > 4)
-        filter = 4;
-
-    driveObj.linear.x = filter / 10;
-  }
-
-  driver.publish(driveObj);
-
+double lowpassFilter(double rawData) {
+    smoothData = smoothData - (LPF_Beta * (smoothData - rawData));
+    return smoothData;
 }
+
+
+//drives the robot
+void drive(double speed, double rotation){
+    geometry_msgs::Twist driveObj;
+    driveObj.angular.z = rotation;
+    driveObj.linear.x = speed;
+    ROS_INFO("x: [%f], z: [%f]", rotation, speed);
+    driver.publish(driveObj);
+}
+
+
+//Kalman
+double estimate = 1; //or initial estimate (needs initialisation, could be anything)
+double errorEstimate = 0.8; //or initial estimate (needs initialisation, give a good range)
+double previousEstimate = estimate; // for init it is the same as the estimate
+double errorMeasurement = 0.8; //(needs initialisation)
+double measurement; //needs initialisation with by using new measurements
+int loops = 2000; // number of maximum cycles for the kalman loop
+int loopCount = 0;
+bool newData;
+
+void kalmanFilter(double measurement, double speed) {
+    double errorEstimate = 0.8;
+    while(newData){
+        loopCount++;
+
+        double kalmanGain = errorEstimate / (errorEstimate + errorMeasurement);
+
+        estimate = previousEstimate + kalmanGain * (measurement - previousEstimate);
+        errorEstimate = (1 - kalmanGain) * errorEstimate;
+
+        ROS_INFO("prediction: %f \t measured value: %f \t diff: %f \n", estimate, measurement, (estimate - measurement));
+        previousEstimate = estimate;
+        double rotation = estimate;
+        ros::spinOnce(); //get new messages, if so trackedCallback gets called;
+        ROS_INFO("loop %i", loopCount);
+        drive(speed / 10, rotation);
+        if(loopCount > 10 ){
+			newData = false; 
+		} 	
+		break;
+	}
+}
+
+
+void trackedCallback(const vision::TrackedPositionConstPtr& msg) {
+    //speed
+    newData = true;
+    loopCount = 0;
+	ROS_INFO("TRACKED CALLBACK");
+    double speed = lowpassFilter(msg->z);
+    if (speed > 4)
+        speed = 4;
+
+	//rotation
+    kalmanFilter(-msg->x, speed);
+}
+
 
 int main(int argc, char **argv) {
-	ROS_INFO("Starting the node");
+  ROS_INFO("Starting the node");
 
-	ros::init(argc, argv, "angular_tracker");
-	ROS_INFO("ros::init done");
+  ros::init(argc, argv, "angular_tracker");
+  ROS_INFO("ros::init done");
 
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("/vision/tracked_position", 100, trackedCallback);
-	driver = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 100);
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("/vision/tracked_position", 100, trackedCallback);
+  driver = n.advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 100);
 
+  ros::spin();
 
-    ros::spin();
-
-
-	ROS_INFO("Exiting the node");
-	return 0;
+  ROS_INFO("Exiting the node");
+  return 0;
 }
-
